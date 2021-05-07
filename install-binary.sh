9e29991c9e6b2887cb971efb7fb30a6fe44bab26
koyu <me@koyu.space>
2021-05-07 10:55:13 +0200

    Add install and removal scripts

---
+++ install-binary.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo wget -O /usr/bin/com.github.koyuspace.fossil https://github.com/koyuspace/fossil/releases/download/v1.1/com.github.koyuspace.fossil
+sudo chmod +x /usr/bin/com.github.koyuspace.fossil
+sudo wget -O /usr/share/applications/com.github.koyuspace.fossil.desktop https://raw.githubusercontent.com/koyuspace/fossil/main/com.github.koyuspace.fossil.desktop.in
+xdg-mime default com.github.koyuspace.fossil.desktop x-scheme-handler/gemini
+xdg-mime default com.github.koyuspace.fossil.desktop x-scheme-handler/gopher
---
+++ remove-binary.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo rm -f /usr/bin/com.github.koyuspace.fossil
+sudo rm -f /usr/share/applications/com.github.koyuspace.fossil.desktop
