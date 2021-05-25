6c2961d1b23b95c186bdc7f7b72b7ab46216ea83
koyu <me@koyu.space>
2021-05-25 11:33:45 +0200

    Bump version

--- install-binary.sh
+++ install-binary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo wget -O /usr/bin/com.github.koyuspace.fossil https://github.com/koyuspace/fossil/releases/download/v1.1/com.github.koyuspace.fossil
+sudo wget -O /usr/bin/com.github.koyuspace.fossil https://github.com/koyuspace/fossil/releases/download/v1.2/com.github.koyuspace.fossil
 sudo chmod +x /usr/bin/com.github.koyuspace.fossil
 sudo wget -O /usr/share/applications/com.github.koyuspace.fossil.desktop https://raw.githubusercontent.com/koyuspace/fossil/main/com.github.koyuspace.fossil.desktop.in
 xdg-mime default com.github.koyuspace.fossil.desktop x-scheme-handler/gemini
